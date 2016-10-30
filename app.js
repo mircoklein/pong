fb83621ecbfa2287134d10c5d4347af5d91b95cb
Gerome Bochmann <gerome.bochmann@gmail.com>
2016-10-30 18:31:56 +0100

    Fixed flickering paddle

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+/*globals angular */
+
 angular.module('pong', []);
-angular.module("pong")
-.controller("CanvasController", CanvasController);
+angular.module('pong')
+.controller('CanvasController', CanvasController);
 function CanvasController (){
     var vm = this;
+    vm.disableRadios = false;
     vm.start = function(){
        pong();
-    }
+       vm.disableRadios = true;
+    };
 }
 
